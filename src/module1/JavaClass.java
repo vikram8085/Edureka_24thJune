dc1e6ef97fc1b6e5eb6c344c5f89fc82f6348a85
vikram8085 <vikramaditya.muppaneni@gmail.com>
2019-06-27 16:59:23 +0530

    changes in java class file

--- src/module1/JavaClass.java
+++ src/module1/JavaClass.java
@@ -3,7 +3,7 @@ package module1;
 public class JavaClass {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to VIKRAM");
+		System.out.println("Welcome to vikram");
 
 	}
 
